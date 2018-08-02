c231c4c5ef613e69860afaf65eb1d911c5d0d8be
Mason117 <zxymason117@outlook.com>
2018-08-02 12:04:01 +0100

    VE

---
+++ SupermarketG11/Assets/G11Scripts/Fiona/ViveControllerInput.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViveControllerInput : MonoBehaviour
+{
+    private SteamVR_TrackedObject trackedObj;
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+
+        //Get the shopping list GameObject and hide it
+        GameObject.Find("ShoppingList").GetComponent<Renderer>().enabled = false;
+
+        //Get the list of shoppinglist items and hide them
+        for(int i = 0; i < GameObject.Find("ShoppingList").transform.childCount; i++)
+        {
+            GameObject.Find("ShoppingList").transform.GetChild(i).GetComponent<Renderer>().enabled = false;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Controller.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            //Toggle visibility of shopping list and items when the user presses the touchpad on the controller
+            GameObject.Find("ShoppingList").GetComponent<Renderer>().enabled = !GameObject.Find("ShoppingList").GetComponent<Renderer>().enabled;
+            var inventory = GetComponent<ProductInventory>();
+
+            for (int i = 0; i < GameObject.Find("ShoppingList").transform.childCount; i++)
+            {
+                var id = GameObject.Find("ShoppingList").transform.GetChild(i).GetComponent<ProductId>().id;
+                
+                if(inventory.IsShoppingListItem(id))
+                {
+                    GameObject.Find("ShoppingList").transform.GetChild(i).GetComponent<Renderer>().enabled = !GameObject.Find("ShoppingList").transform.GetChild(i).GetComponent<Renderer>().enabled;
+                }                              
+            }
+        }
+    }
+}
---
+++ SupermarketG11/Assets/G11Scripts/Mason/Tmove.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tmove : MonoBehaviour {
+
+    // Use this for initialization
+    public int touch;
+    public int debug;
+	void Start () {
+        touch = 0;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        //print("  " + other.gameObject.name + "    碰撞了    " + gameObject.name);
+        if (other.gameObject.GetComponent<ProductCode>())
+        {
+            print(" it is a food.");
+            other.gameObject.transform.parent = this.transform;
+        }
+        else
+        {
+            //player 碰撞也要去掉
+            touch = 1;
+        }
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        //print("  " + other.gameObject.name + "  离开了  " + gameObject.name);
+        touch = 0;
+        //other.gameObject.transform.parent = null;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+      
+        print("  " + other.gameObject.name + "    碰撞了    " + gameObject.name);
+        if (other.gameObject.GetComponent<ProductCode>())
+        {
+            print(" it is a food.");
+            other.gameObject.transform.parent = this.transform;
+        }
+
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+     
+        print("  " + other.gameObject.name + "  离开了  " + gameObject.name);
+        touch = 0;
+        other.gameObject.transform.parent =null;
+    }
+}
